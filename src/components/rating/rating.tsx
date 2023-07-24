733bea8e3f4c8e12e93917aea76133ef80b6e824
Arjit singh <arjitsingh8@gmail.com>
2023-07-24 13:44:16 +0530

    added rating component page and widget

---
+++ src/components/rating/rating.tsx
@@ -0,0 +1,55 @@
+import { StarIcon } from "@heroicons/react/24/outline"
+import { FunctionComponent, useState } from "react"
+
+interface props extends React.HtmlHTMLAttributes<HTMLSpanElement>{
+    children : React.ReactNode,
+    defaultValue ?: number,
+    max  ?:number
+}
+
+export const RatingWidget: FunctionComponent<props> = (props:props)=>{
+    const starArray = new Array(5).fill(props.defaultValue)
+    const selectedMap = new Array(props.defaultValue)
+    const [selected, setSelected] = useState(new Array(props.defaultValue).fill(false))
+
+    const [hovered, setHovered] = useState(new Array(props.defaultValue).fill(false))
+    const handleMouseEnter = (idx:number) =>{
+        const updatedStar = hovered.map((star,i)=>i<=idx?true:false)
+        setHovered(updatedStar)
+    }
+    const handleMouseLeave = (idx:number)=>{
+        setHovered(selected)
+    }
+    const selectStart = (idx:number)=>{
+        const updatedStar = selected.map((star,i)=>i<=idx?true:false)
+        setSelected(updatedStar)
+    }
+    return (
+        <>
+            <span>
+                {
+                    props.children
+                }
+            </span>
+            <span>
+                {
+                    starArray.map((star,i)=>{
+                        return <>
+                        <StarIcon height={20} width={20} style={{color: selected[i] || hovered[i] ? 'yellow' : null}}
+                         onMouseEnter={(e)=>handleMouseEnter(i)}
+                         onMouseLeave={(e)=>{handleMouseLeave(i)}}
+                         onClick={()=>{selectStart(i)}}
+                         key={`${i}-star`}
+                         />
+                        </>
+                    })
+
+                }
+            </span>
+        </>
+    )
+}
+
+RatingWidget.defaultProps = {
+    defaultValue : 5
+}
--- src/pages/rating/ratingPage.tsx
+++ src/pages/rating/ratingPage.tsx
@@ -1,8 +1,23 @@
+import {RatingWidget} from "../../components/rating/rating";
+
 export default function RatingPage(){
     return (
         <>
          <div className="container">
             <section className="section-display">
+            <h3 className="color-light">Rating widge</h3>
+            <h5 className="color-light">With Hover feedback and selection</h5>
+            <pre>
+                <code>
+                    variant="text"
+                </code>
+            </pre>
+            <RatingWidget>
+                rating
+            </RatingWidget>
+            </section>
+         
+            {/* <section className="section-display">
             <h3 className="color-light">Rating widge</h3>
             <h5 className="color-light">With ripple effect</h5>
             <pre>
@@ -10,8 +25,10 @@ export default function RatingPage(){
                     variant="text"
                 </code>
             </pre>
-            </section>
-
+            <RatingWidget defaultValue={20}>
+                rating
+            </RatingWidget>
+            </section> */}
           </div>
         </>
     )
