ec7cf1446c2d0c2bf2d3bbf2d51018389e9bd110
Arjit singh <arjitsingh8@gmail.com>
2024-08-02 22:11:57 +0530

    removed unneccessary comment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,24 +6,19 @@ import App from "./App.tsx";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import * as React from "react";
-// describe("checking",()=>{
-//     it("checking sum",()=>{
-//         expect(2+2).toBe(4)
-//     })
-// })
 
 describe(App, () => {
-    it("check for heading",async()=>{
-        const {getByText} = render(<App/>)
-        const text = getByText("Component Library")
-        expect(text.className).toContain("page_title")
-    })
-    
-    it("should contain heading of Component Library", async()=>{
-        const {getAllByText} = render(<App/>)
-        const text = getAllByText("Component Library")
-        for(let node of text){
-            expect(node.textContent).toContain("Component Library")
-        }
-    })
+  it("check for heading", async () => {
+    const { getByText } = render(<App />);
+    const text = getByText("Component Library");
+    expect(text.className).toContain("page_title");
+  });
+
+  it("should contain heading of Component Library", async () => {
+    const { getAllByText } = render(<App />);
+    const text = getAllByText("Component Library");
+    for (let node of text) {
+      expect(node.textContent).toContain("Component Library");
+    }
+  });
 });
